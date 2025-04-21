fe6b0d37c4641ad35976cf0204b4bfc421314715
Fábio Berbert de Paula <fberbert@gmail.com>
2025-04-21 06:51:59 -0300

    ci workflow updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tux-gpt",
-    version="0.1.3",
+    version="0.1.4",
     description="An interactive terminal tool using GPT",
     long_description=long_description,
     long_description_content_type="text/markdown",
