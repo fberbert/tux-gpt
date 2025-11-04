e3f4eebbec8d10e2b6a8b6ad648d66fc252958ca
Fábio Berbert de Paula <fberbert@gmail.com>
2025-11-04 20:38:33 -0300

    update version to 0.2.4 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tux-gpt",
-    version="0.2.3",
+    version="0.2.4",
     description=(
         "An interactive terminal tool using GPT, "
         "with web search capabilities."
